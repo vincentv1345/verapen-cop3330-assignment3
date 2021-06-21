6f352bfa20c466bbf0afa3c562fc5603e77f93be
Vincent <vincentverapen2012@gmail.com>
2021-06-20 23:31:09 -0400

    6th commit

--- src/test/java/ex45/testEx45.java
+++ src/test/java/ex45/testEx45.java
@@ -1,4 +1,15 @@
 package ex45;
 
+import org.junit.jupiter.api.Test;
+
 public class testEx45 {
+    @Test
+    public String replace(String r){
+        r = "Hi vincent utilizes a pencil";
+        String newString = "";
+        if(r.contains("utilize")){
+            newString = r.replace("utilize", "use");
+        }
+        return newString;
+    }
 }
